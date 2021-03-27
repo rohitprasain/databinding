fe97ac8a6aadf6c4773667d56f490c5046696305
rohit prasain <rohit1@gmail.com>
2021-03-27 14:31:08 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todos, setTodos] = useState([
+    { id: 1, task: "task11" },
+    { id: 2, task: "task2" },
+  ]);
+
+  const onchangeHandler = (id) => (e) => {
+    console.log(id);
+
+    let copyTodo = [...todos];
+    let indexCopyTodo = { ...copyTodo[id] };
+    // console.log(indexCopyTodo)
+    indexCopyTodo.task = e.target.value;
+
+    copyTodo[id] = indexCopyTodo;
+
+    setTodos(copyTodo);
+    // console.log(newTask)
+    // setTodos( {...todos[id+1] ,task : e.target.value})
+    // console.log(name)
+  };
+
+  return todos.map((todo, ind) => {
+    return (
+
+        <div className="App" key={ind}>
+          <div className="original">
+            {/* {todo.id}
+                  {todo.task} */}
+            <h2>{JSON.stringify(todo)}</h2>
+          </div>
+          <div className="changed">
+            <input
+              type="text"
+              name="task"
+              onChange={onchangeHandler(ind)}
+              placeholder={todo.task}
+            />
+          </div>
+        </div>
+
+    );
+  });
 }
 
 export default App;
